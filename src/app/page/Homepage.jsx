ec2770bb57f598bd5a125eef297528dcd52c1cb7
ShadowConYT <iamajay6122@gmail.com>
2024-01-05 12:21:43 +0530

    Update fonts and navbar styles

--- src/app/page/Homepage.jsx
+++ src/app/page/Homepage.jsx
@@ -8,12 +8,13 @@ import GitAlone from '../assets/Card/GitAlone.png';
 import Proj from '../assets/Card/Proj.png';
 import Social from '../assets/Card/Social.jpg';
 
-import { Card, CardBody, CardHeader, CardFooter } from '@nextui-org/react';
-import { Button } from '@nextui-org/react';
+import localFont from 'next/font/local';
 
 import '../styles/homepage.css'
 
-// floating icons
+const myFont = localFont({
+  src:'../assets/fonts/Attena.otf'
+})
 export default function Homepage(){
 
   const data = ['Designer', 'Developer', 'ML Enthusiast'];
@@ -26,7 +27,7 @@ export default function Homepage(){
 
   return (
     <div className='h-dvh bg-gradient-to-b from-slate-700 via-slate-600 to-slate-700 overflow-hidden inset-0'>
-      <div className='z-10 mx-10 w-auto lg:absolute lg:transform lg:translate-x-1/2'>
+      <div className='z-10 md:mx-10 w-auto lg:absolute lg:transform lg:translate-x-1/2'>
           <Navbar />
       </div>
       <div className='lg:absolute bottom-0 lg:w-[700px] lg:z-10'>
@@ -35,7 +36,7 @@ export default function Homepage(){
       <div className='mx-10 lg:h-screen w-full lg:flex lg:items-center lg:justify-center'>
             <div className='w-[450px] translate-x-1/2'>
               <p className='text-white justify-center lg:pl-[1.5vw] lg:text-4xl '>Hi 👋, I <span className='text-[--secondary-color]'>am</span></p>
-              <h1 className='text-white lg:text-9xl' >Ajay</h1>
+              <h1 className={`text-white lg:text-9xl ${myFont.className} uppercase`} >Ajay</h1>
               <p className='text-white lg:py-2 lg:text-5xl'>I'm a <TypeWriter data= {data} /></p>
             </div>
       </div>
