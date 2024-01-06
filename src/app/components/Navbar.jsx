77474eafb5d5d347e284f6314c324e61bbea1c56
ShadowConYT <iamajay6122@gmail.com>
2024-01-06 18:24:58 +0530

    So Far So Good

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,10 +1,9 @@
 'use client'
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import { FaTimes, FaBars } from 'react-icons/fa';
 import { useRef } from 'react';
 import '../styles/navbar.css';
-import SampleLogo from '../assets/SampleLogo.png';
 
 const Navbar = () => {
 
@@ -13,41 +12,67 @@ const Navbar = () => {
     const showBar = () => {
         navRef.current.classList.toggle('responsive_nav');
     }
+    useEffect(() => {
+        let btnContainer = document.getElementById('nav');
+        let btns = btnContainer.getElementsByClassName('aTag');
+    
+        for (let i = 0; i < btns.length; i++){
+            btns[i].addEventListener("click", function(){
+                let current = document.getElementsByClassName('active');
+    
+                if (current.length > 0){
+                    current[0].className = current[0].className.replace(' active', '');
+                }
+    
+                this.className += ' active';
+            })
+        }
+    }, []);
 
-    let btnContainer = document.getElementById('nav');
-    let btns = btnContainer.getElementsByClassName('aTag');
 
-    for (let i = 0; i < btns.length; i++){
-        btns[i].addEventListener("click", function(){
-            let current = document.getElementsByClassName('active');
+    const [isMobileView, setIsMobileView] = useState(false);
 
-            if (current.length > 0){
-                current[0].className = current[0].className.replace(' active', '');
-            }
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobileView(window.innerWidth <= 1024);
+        }
 
-            this.className += ' active';
-        })
-    }
+        // Run once to set initial state
+        handleResize();
+
+        // Set up event listener
+        window.addEventListener('resize', handleResize);
+
+        // Clean up event listener on component unmount
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+        
+
 
   return (
-    <div className='head'>
-        <div className='xs:block:md lg:opacity-0 w-20 md:w-20'>
-            <Image src={SampleLogo} alt='Logo'/>
+    <div className='head md:min-w-9xl' >
+        <div className='xs:block:md lg:opacity-100 md:w-20'>
+            <span>Ajay</span>
         </div>
-
         <nav id='nav' className='font-roz' ref={navRef}> 
             <a className='aTag' href="#">About</a>
             <a className='aTag' href="#">Skills</a>
             <a className='aTag' href="#">Education</a>
             <a className='aTag' href="#">Works</a>
             <a className='aTag' href="#">Contact</a>
-            <button className='nav-btn nav-close-btn text-white' onClick={showBar}>
-                <FaTimes />
-            </button>
+            {isMobileView ? 
+                <button className='nav-btn nav-close-btn text-white' onClick={showBar}>
+                    <FaTimes />
+                </button> : null  
+            }
+            
         </nav>
-        <button onClick={showBar} className="text-white nav-btn">
-            <FaBars />
-        </button>
+        {isMobileView ? 
+            <button onClick={showBar} className="text-white nav-btn">
+                <FaBars />
+            </button>
+            : null
+        }
     </div>
   )
 }
--- src/app/page/Homepage.jsx
+++ src/app/page/Homepage.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Navbar from '../components/Navbar';
-import TypeWriter from '../components/Typewriter';
 import Image from 'next/image';
 import AssetImg from '../assets/AssetImg.png';
 import GitHub from '../assets/Card/GitHub.png';
@@ -13,7 +12,7 @@ import localFont from 'next/font/local';
 import '../styles/homepage.css'
 
 const myFont = localFont({
-  src:'../assets/fonts/Attena.otf'
+  src:'../assets/fonts/ClassyVogue.otf'
 })
 export default function Homepage(){
 
@@ -26,21 +25,20 @@ export default function Homepage(){
   ]
 
   return (
-    <div className='h-dvh bg-gradient-to-b from-slate-700 via-slate-600 to-slate-700 overflow-hidden inset-0'>
-      <div className='z-10 md:mx-10 w-auto lg:absolute lg:transform lg:translate-x-1/2'>
+    <div className='h-screen bg-[#2D3250] overflow-hidden inset-0 flex flex-col items-center'>
+      <div className='z-10'>
           <Navbar />
       </div>
-      <div className='lg:absolute bottom-0 lg:w-[700px] lg:z-10'>
-        <Image src={AssetImg} />
+      <div className='mx-10 -gap-y-20 md:max-w-7xl lg:flex lg:flex-col lg:h-full lg:items-center lg:justify-center'>
+        <p className='text-white flex lg:pl-[1.5vw] lg:text-4xl lg:bottom-0 m-0 leading-none'>Hi 
+          <span>
+          <picture>
+            <source srcSet="https://fonts.gstatic.com/s/e/notoemoji/latest/1f44b/512.webp" type="image/webp" />
+            <img src="https://fonts.gstatic.com/s/e/notoemoji/latest/1f44b/512.gif" alt="👋" width="32" height="32" />
+          </picture>
+          </span>, <span className='text-[--secondary-color] pl-2'>I am</span></p>
+        <span className={`text-white lg:bottom-0 m-0 leading-none lg:text-[400px] ${myFont.className} `} >Ajay</span>
       </div>
-      <div className='mx-10 lg:h-screen w-full lg:flex lg:justify-center'>
-            <div className=''>
-              <p className='text-white justify-center lg:pl-[1.5vw] lg:text-4xl '>Hi 👋, I <span className='text-[--secondary-color]'>am</span></p>
-              <h1 className={`text-white lg:bottom-0 m-0 p-0 lg:text-[400px] ${myFont.className} uppercase`} >Ajay</h1>
-              {/*<p className='text-white lg:py-2 lg:text-5xl'>I'm a <TypeWriter data= {data} /></p>*/}
-            </div>
-      </div>
-
     </div>
   )
 }
