7e08532303ca845a907acbe1da857fe87af8dbdc
ShadowConYT <iamajay6122@gmail.com>
2024-01-07 15:30:40 +0530

    Add font link and update homepage layout

--- src/app/page/Homepage.jsx
+++ src/app/page/Homepage.jsx
@@ -24,18 +24,28 @@ export default function Homepage(){
       <div className='z-20'>
           <Navbar />
       </div>
-      <div className='z-10 mx-10 -gap-y-20 md:min-w-7xl md:flex md:flex-col md:h-full md:items-center md:justify-center'>
-        <p className='text-white md:text-5xl flex lg:pl-[1.5vw] lg:text-4xl lg:bottom-0 m-0 leading-none'>Hi 
-          <span>
-          <picture>
-            <source srcSet="https://fonts.gstatic.com/s/e/notoemoji/latest/1f44b/512.webp" type="image/webp" />
-            <img src="https://fonts.gstatic.com/s/e/notoemoji/latest/1f44b/512.gif" alt="👋" width="32" height="32" />
-          </picture>
-          </span>, <span className='text-[--secondary-color] pl-2'>I am</span></p>
-          <span className={`text-white md:text-[250px] lg:bottom-0 mb-5 leading-none lg:text-[300px] ${myFont.className} `} >Ajay</span>
+      <div className='z-10 mx-10 relative -gap-y-20 md:max-w-6xl md:grid md:grid-cols-2 md:align-middle md:h-full '>
+        <div className='md:flex md:flex-col md:justify-center'>
+          <p className='text-white md:text-5xl flex lg:bottom-0 m-0 leading-none'>Hi 
+            <span>
+            <picture>
+              <source srcSet="https://fonts.gstatic.com/s/e/notoemoji/latest/1f44b/512.webp" type="image/webp" />
+              <img src="https://fonts.gstatic.com/s/e/notoemoji/latest/1f44b/512.gif" alt="👋" width="32" height="32" />
+            </picture>
+            </span>, <span className='pl-2'>I am</span>
+            <span className={`lg:bottom-0 pl-3 mb-5  text-[--secondary-color] font-bold font-[montserrat] `} >Ajay</span>
+          </p>
+          <p className='text-gray-200 tracking-wider'>
+            Tired of bouncing between siloed teams or struggling to communicate your vision? I'm here to bridge the gap. As a passionate Developer and Designer, I leverage the power of both worlds to create stunning UIs informed by data-driven insights.
+            </p>
+        </div>
+
+        <div>
+          <Image src={AssetImg} className='absolute -bottom-40 md:blur-sm lg:blur-0 w-auto right-0 md:bottom-0 scale-x-[-1] object-scale-down max-h-full z-0' />
+        </div>
+        
       </div>
     </div>
-    <Image src={AssetImg} className='absolute -bottom-40 md:blur-sm lg:blur-0 w-auto right-0 md:bottom-0 scale-x-[-1] object-scale-down max-h-full z-0' />
     </>
   )
 }
