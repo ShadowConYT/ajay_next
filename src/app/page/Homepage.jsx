f74e45e73331711820352b4c10c4e2590a96a331
ShadowConYT <iamajay6122@gmail.com>
2024-02-04 00:30:28 +0530

    Animation Added

--- src/app/page/Homepage.jsx
+++ src/app/page/Homepage.jsx
@@ -32,26 +32,52 @@ const Homepage = ({ data }) => {
         <div className='flex md:flex-col md:justify-center'>
           <div>
             <div className='font-[montserrat] font-semibold flex text-white text-2xl text-center justify-center md:justify-normal  md:text-4xl lg:bottom-0 m-0 leading-none'>
-              <motion.span className=''>Hi</motion.span>
-                <span className='p-0 m-0 top-1'>
+              <motion.span
+                initial = {{opacity:0, y: 50, duration: 5}}
+                animate = {{opacity:1, y:0, duration: 5}}
+                whileInView={{opacity:1, y:0, duration: 5, transition: {delay:0.5}}}
+                className=''>Hi</motion.span>
+                <motion.span 
+                  initial = {{opacity:0, y: 50, duration: 5}}
+                  animate = {{opacity:1, y:0, duration: 5}}
+                  whileInView={{opacity:1, y:0, duration: 5, transition: {delay:0.5}}}
+                  className='p-0 m-0 top-1'>
+                  
                   <picture>
                     <source srcSet="https://fonts.gstatic.com/s/e/notoemoji/latest/1f44b/512.webp" type="image/webp" />
                     <img src="https://fonts.gstatic.com/s/e/notoemoji/latest/1f44b/512.gif" alt="👋" className='w-6 md:w-8 lg:w-9' />
                   </picture>
-                </span>, <span className='pl-2'>I am</span>
-                <span className={`lg:bottom-0 pl-1.5 md:pl-3 mb-5  text-[--secondary-color] font-extrabold uppercase`} >{name}</span>
+                </motion.span>
+                <motion.span 
+                  initial = {{opacity:0, y: 50, duration: 5}}
+                  animate = {{opacity:1, y:0, duration: 5}}
+                  whileInView={{opacity:1, y:0, duration: 5, transition: {delay:0.8}}}
+                  className='pl-2'>, I am</motion.span>
+                <motion.span 
+                  initial = {{opacity:0, y: 50, duration: 5}}
+                  animate = {{opacity:1, y:0, duration: 5}}
+                  whileInView={{opacity:1, y:0, duration: 5, transition: {delay:1.1}}}
+                  className={`lg:bottom-0 pl-1.5 md:pl-3 mb-5  text-[--secondary-color] font-extrabold uppercase`} >{name}</motion.span>
             </div>
             
 
-              <motion.p className='text-justify md:text-left md:w-auto text-gray-200 tracking-wider'>
+              <motion.p
+                initial = {{opacity:0, y: 50, duration: 5}}
+                animate = {{opacity:1, y:0, duration: 5}}
+                whileInView={{opacity:1, y:0, duration: 5, transition: {delay:1.4}}}
+                className='text-justify md:text-left md:w-auto text-gray-200 tracking-wider'>
                 {about}
               </motion.p>
 
-              <div className='flex justify-center md:block'>
+              <motion.div 
+                initial = {{opacity:0, y: 50, duration: 5}}
+                animate = {{opacity:1, y:0, duration: 5}}
+                whileInView={{opacity:1, y:0, duration: 5, transition: {delay:1.7}}}
+                className='flex justify-center md:block'>
                 <button className="button text-center mt-2 md:mt-5" onClick={ResumeLink}>
                   Resume
                 </button>
-              </div>
+              </motion.div>
               
               
 
